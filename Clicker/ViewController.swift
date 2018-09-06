8e76379c31e5e65d1a430e34d854ceda520ae56b
Simone <simonecaplin@gmail.com>
2018-09-06 19:23:56 +0100

    Initial Commit

---
+++ Clicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clicker
+//
+//  Created by Simone Smith on 06/09/2018.
+//  Copyright © 2018 Simone Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
